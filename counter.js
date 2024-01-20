7944ae742eafd7df74138ebb2990fd39d51953b3
unknown <joshuajulius2030@gmail.com>
2024-01-21 00:50:21 +0100

    successful

---
+++ counter.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function () {
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("increaseBtn").onclick = function () {
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("resetBtn").onclick = function () {
+    count = 0;;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+const numbers = [1, 2, 3];
+numbers[10] = 11;
+
+console.log(numbers);
